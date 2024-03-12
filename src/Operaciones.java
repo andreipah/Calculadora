c9355ea3d400fcf7de32ad6287bb75ebe73a818d
andrei.pah <andrei.pahomie95@gmail.com>
2024-03-12 19:40:32 +0100

    Añadido metodo para restar

--- src/Operaciones.java
+++ src/Operaciones.java
@@ -10,3 +10,15 @@ public class Operaciones {
     public static double sumar(double num1, double num2) {
         return num1 + num2;
     }
+
+    /**
+     * Operacion de suma de dos numeros
+     *
+     * @param num1 Primer numero a restar
+     * @param num2 Segundo numero a restar
+     * @return Devuelve el resultado
+     */
+    public static double restar(double num1, double num2) {
+        return num1 - num2;
+    }
+}
