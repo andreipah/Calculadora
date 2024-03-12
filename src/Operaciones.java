7abda0f691c0654003ff37d4db3f36128ea2fc04
andrei.pah <andrei.pahomie95@gmail.com>
2024-03-12 19:42:12 +0100

    Añadido metodo para multiplicar

--- src/Operaciones.java
+++ src/Operaciones.java
@@ -21,4 +21,14 @@ public class Operaciones {
     public static double restar(double num1, double num2) {
         return num1 - num2;
     }
+    /**
+     * Operacion de multiplicacion de dos numeros
+     *
+     * @param num1 Primer numero a multiplicar
+     * @param num2 Segundo numero a multiplicar
+     * @return Devuelve el resultado
+     */
+    public static double multiplicar(double num1, double num2) {
+        return num1 * num2;
+    }
 }
