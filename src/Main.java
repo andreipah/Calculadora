c3785e0657d08a98d9950b6383d827383cfd9f06
andrei.pah <andrei.pahomie95@gmail.com>
2024-03-12 19:35:37 +0100

    Añadido menu de entrada y salida de datos

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+// Clase principal que implementa una calculadora simple
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+        DecimalFormat df = new DecimalFormat("#.###");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Bucle principal del programa que permite al usuario realizar varias operaciones
+        do {
+            System.out.println("Calculadora - Opciones:");
+            System.out.println("1.- Sumar");
+            System.out.println("2.- Restar");
+            System.out.println("3.- Multiplicar");
+            System.out.println("4.- Dividir");
+            System.out.println("0.- Salir");
+            System.out.print("Que opcion deseas realizar?: ");
+            opcion = scanner.nextInt();
+            // Condicional que verifica si la entrada es valida
+            if (opcion >= 1 && opcion <= 4) {
+                System.out.print("Ingresa el primer numero: ");
+                double num1 = scanner.nextDouble();
+                System.out.print("Ingresa el segundo numero: ");
+                double num2 = scanner.nextDouble();
+
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Resultado: " + df.format(Operaciones.sumar(num1, num2)));
+                        break;
+                    case 2:
+                        System.out.println("Resultado: " + df.format(Operaciones.restar(num1, num2)));
+                        break;
+                    case 3:
+                        System.out.println("Resultado: " + df.format(Operaciones.multiplicar(num1, num2)));
+                        break;
+                    case 4:
+                        System.out.println("Resultado: " + df.format(Operaciones.dividir(num1, num2)));
+                        break;
+                }
+            } else if (opcion != 0) {
+                System.out.println("Opcion no valida. Por favor, elige una opcion valida.");
+            }
+
+        } while (opcion != 0);
+
+        System.out.println("Hasta la vista");
+
     }
 }
