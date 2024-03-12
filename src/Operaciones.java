7a28cc80d29f887917f6458471b264accec04065
andrei.pah <andrei.pahomie95@gmail.com>
2024-03-12 19:37:48 +0100

    Añadido metodo para sumar

--- src/Operaciones.java
+++ src/Operaciones.java
@@ -1,2 +1,12 @@
+
 public class Operaciones {
-}
+    /**
+     * Operacion de suma de dos numeros
+     *
+     * @param num1 Primer numero a sumar
+     * @param num2 Segundo numero a sumar
+     * @return Devuelve el resultado
+     */
+    public static double sumar(double num1, double num2) {
+        return num1 + num2;
+    }
